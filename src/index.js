34ea8de6b5a467dfb5f8e37493f86578519d37b7
gunjuKo <gunju@sk.com>
2017-12-26 18:07:13 +0900

    Header 부분 구현

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
-// node_modules 폴더에 설치된 모듈 중 리액트라는 라이브러리를 찾고
-// 그 라이브러리를 불러와 이를 React 변수에 할당해라
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDom from 'react-dom';
-import Searchbar from './components/search_bar';
-
-const API_KEY = 'AIzaSyA7NepuWGXCP1-0Jm-o1jg0TlbJzOvaMYE';
+import Header from './components/header';
 
 // Create a new component, This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <Searchbar/>
-        </div>
-    );
+class App extends Component {
+
+    render() {
+        return (
+            <div>
+                <Header/>
+            </div>
+        );
+    }
 };
 
 // Take this component's generated HTML and put it on the page (in the DOM)
