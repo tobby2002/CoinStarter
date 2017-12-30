c474c9cea87fbb05db0f86b9720d7d8497ce1eac
gunjuKo <gunju@sk.com>
2017-12-30 17:01:13 +0900

    HTML 수정

--- src/components/main.js
+++ src/components/main.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Main = () => {
     return (
-        <div className="col-sm-8 text-left">
+        <div className="col-sm-10 text-left">
             <h1> CoinStarter </h1>
             <p> CoinStater에서 전체적인 코인 상황을 파악하세요 </p>
 
-            <h3>Test</h3>
+            <h3>ABOUT</h3>
             <p>
-                CoinStater는 Bitfinex의 마진거래 포지션의 비율을 확인해주는 사이트입니다.
-                Bitcoin 마진 확인과 Altcoin 마진 확인을 눌러 확인해 보세요.
+                CoinStater는 Bitfinex의 마진거래 포지션의 비율을 확인해주는 사이트입니다. <br/>
+                Bitcoin 마진 확인 <br/>
+                Altcoin 마진 확인 <br/>
+                메뉴에서 확인하세요.
             </p>
         </div>
     );
--- src/components/navigation-bar.js
+++ src/components/navigation-bar.js
@@ -5,22 +5,15 @@ const NavigationBar = () => {
         <nav className="navbar navbar-inverse">
             <div className="container-fluid">
                 <div className="navbar-header">
-                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                        <span className="icon-bar"></span>
-                    </button>
-                    <a className="navbar-brand" href="#">Logo</a>
+                    <p className="navbar-brand">CoinStater</p>
                 </div>
                 <div className="collapse navbar-collapse" id="myNavbar">
                     <ul className="nav navbar-nav">
-                        <li className="active"><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Projects</a></li>
+                        <li><a href="#">Home</a></li>
                         <li><a href="#">Contact</a></li>
                     </ul>
                     <ul className="nav navbar-nav navbar-right">
-                        <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+                        <li><a href="#">Login</a></li>
                     </ul>
                 </div>
             </div>
